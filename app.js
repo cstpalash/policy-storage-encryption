610a5d919c5f5fe2e30a869accc175bfad7d8238
cstpalash <cstpalash@gmail.com>
2021-09-12 23:35:08 +0530

    BDD

--- app.js
+++ app.js
@@ -1,15 +1,94 @@
 exports.handler = async function(event, context) {
   
-  event.Records.forEach(record => {
-    console.log(record.body)
-  })
-  console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
-  console.log('## CONTEXT: ' + serialize(context))
-  console.log('## EVENT: ' + serialize(event))
+  console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env));
+  console.log('## CONTEXT: ' + serialize(context));
+  console.log('## EVENT: ' + serialize(event));
   
-  
-  return { hello : "WORLD" };
+
+  if(exports.checkForEncryption(event) === false)
+    return { valid : false, message : 'storage is not encrypted' };
+
+  var expectedAlgo = getExpectedEncryptionAlgorithm(event);
+
+  if(exports.checkForEncryptionAlgorithm(event) != expectedAlgo)
+    return { valid : false, message : `not encrypted with expected algorithm : ${expectedAlgo}` };
+
+  return { valid : true, message : '' };
 }
+
+exports.checkForEncryption = function(event){
+
+  switch(event.key){
+    case 'aws-s3':
+      return checkForEncryption_AWS_S3(event);
+      break;
+    default:
+      return false;
+      break;
+  }
+
+}
+
+exports.checkForEncryptionAlgorithm = function(event){
+
+  switch(event.key){
+    case 'aws-s3':
+      return checkForEncryptionAlgorithm_AWS_S3(event);
+      break;
+    default:
+      return '';
+      break;
+  }
+
+  return '';
+
+}
+
+function getExpectedEncryptionAlgorithm(event){
+  switch(event.key){
+    case 'aws-s3':
+      return 'aws:kms';
+      break;
+    default:
+      return '';
+      break;
+  }
+}
+
+// ################################# AWS S3 RUNNING - start #################################
+
+function checkForEncryption_AWS_S3(event){
+
+  if(event.metadata &&
+    event.metadata.ServerSideEncryptionConfiguration &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules.length > 0 &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules[0].BucketKeyEnabled === true){
+
+    return true;
+  }
+
+  return false;
+
+}
+
+function checkForEncryptionAlgorithm_AWS_S3(event){
+
+  if(event.metadata &&
+    event.metadata.ServerSideEncryptionConfiguration &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules.length > 0 &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules[0].ApplyServerSideEncryptionByDefault &&
+    event.metadata.ServerSideEncryptionConfiguration.Rules[0].ApplyServerSideEncryptionByDefault.SSEAlgorithm){
+
+    return event.metadata.ServerSideEncryptionConfiguration.Rules[0].ApplyServerSideEncryptionByDefault.SSEAlgorithm;
+  }
+
+  return '';
+  
+}
+
+// ################################# AWS S3 RUNNING - end #################################
 
 var serialize = function(object) {
   return JSON.stringify(object, null, 2)
--- features/policy.js
+++ features/policy.js
@@ -1,14 +1,35 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
+const implementation = require('../app.js');
+
+
+
+// ################################# AWS S3 RUNNING - start #################################
+
 Given('encryption configuration of AWS S3 bucket in {string}', function (environment) {
+
+	this.policyMetadata = require('./mock/aws.s3.encryption.json');
 
 });
 
 When('checked for BucketKeyEnabled and SSEAlgorithm', function () {
 
+	var event = {
+		key: 'aws-s3',
+		metadata: this.policyMetadata
+	};
+
+	this.actualEncryption = implementation.checkForEncryption(event);
+	this.actualEncryptionAlgorithm = implementation.checkForEncryptionAlgorithm(event);
+
 });
 
-Then('BucketKeyEnabled should be {string} and SSEAlgorithm should be {string}', function (resultBucketKeyEnabled, resultSSEAlgorithm) {
+Then('BucketKeyEnabled should be {string} and SSEAlgorithm should be {string}', function (expectedBucketKeyEnabled, expectedSSEAlgorithm) {
+
+	assert.strictEqual(this.actualEncryption.toString(), expectedBucketKeyEnabled);
+	assert.strictEqual(this.actualEncryptionAlgorithm, expectedSSEAlgorithm);
 
 });
+
+// ################################# AWS S3 RUNNING - end #################################
